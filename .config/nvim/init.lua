012efacd97b82ee86dc14b79451e42faaded29dc
aliasboink <adrianlaurb@gmail.com>
2025-03-01 22:32:56 +0200

    Change!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -553,7 +553,7 @@ require('lazy').setup({
       local servers = {
         yamlls = {},
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
